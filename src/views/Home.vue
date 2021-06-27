ac00214dde2c64f8731823b9792b1b5dd501dd90
Carl Erik Melhus <carlerik@cartagena.no>
2021-06-27 17:26:21 +0200

    Authentication (Still some errors)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <div>
+      Velkommen til <span>{{ userInfo }}</span>
+    </div>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -8,9 +11,15 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import UserInfoStore from "../app/user-info-store";
 
 export default {
   name: "Home",
+  data: function () {
+    return {
+      userInfo: UserInfoStore.state.cognitoInfo,
+    };
+  },
   components: {
     HelloWorld,
   },
